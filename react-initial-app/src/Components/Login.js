5acb7dd995c1a5dbf47c50788b835dad498f007e
shushanth2003 <bsshu2003@gmail.com>
2025-09-02 09:48:58 +0530

    Feat: Learned about submitted form

--- react-initial-app/src/Components/Login.js
+++ react-initial-app/src/Components/Login.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 function Login(){
-    const[input,setInput]=useState({});
+    const[input,setInput]=useState({phoneno:"+91",gmail:"@gmail.com"});
     const onSubmit=(e)=>{
         const name=e.target.name;
         const value=e.target.value;
@@ -15,9 +15,16 @@ function Login(){
     return(
         <>
             <form onSubmit={onSubmitFile}>
-                <label>Enter the Email : <input type="text" name="gmail" onChange={onSubmit} /></label><br />
+                <label>Enter the Email : <input type="text" name="gmail" onChange={onSubmit} value={input.gmail}/></label><br />
                 <label>Enter the Password : <input type="text" name="password" onChange={onSubmit} /></label><br />
                 <label>Enter the FullName : <input type="text" name="fullname" onChange={onSubmit} /></label><br />
+                <label>Enter the Phone Number : <input type="text" name="phoneno" onChange={onSubmit} value={input.phoneno}/></label> <br />
+                <label>Enter the country  : <select name="country" onChange={onSubmit} value={input.country}>
+                        <option value="India" selected>India</option>
+                        <option value="Austrilia">Austrilia</option>
+                        <option value="Indonesia">Indonesia</option>
+                    </select></label> <br />
+                <label>About me : <textarea name="about" onChange={onSubmit}></textarea></label><br />
                 <input type="submit" value="File Submitted" />
             </form>
         </>
