67fc433f1faf8fd3e0d6cabe20d8edda48c7c1dd
shushanth2003 <bsshu2003@gmail.com>
2025-09-05 19:27:32 +0530

    Feat: Learned about the backend integration

--- mini-ecommerce/backend/app.js
+++ mini-ecommerce/backend/app.js
@@ -6,6 +6,7 @@ dotenv.config({path:path.join(__dirname,'config','config.env')})
 const product=require("./router/product.js")
 const order=require("./router/order.js");
 const connectDB = require('./config/connectDB.js');
+app.use(express.json());
 app.use("/api/v1/",product);
 app.use("/api/v1/",order);
 connectDB();
--- mini-ecommerce/backend/controller/productController.js
+++ mini-ecommerce/backend/controller/productController.js
@@ -3,18 +3,37 @@ const productModel=require("../model/productModel.js")
 
 //Get Product Id
 exports.getProduct=async(req,res,next)=>{
-   const product=await productModel.find({});
-    res.json({
-        success:true,
-        product
-    })
+   try{
+        const product=await productModel.find({});
+        res.json({
+            success:true,
+            product
+        })
+   }catch(error){
+        res.json(
+            {
+                success:false,
+                message:error.message
+            }
+        )
+   }
 }
 
-exports.getProductId=(req,res,next)=>{
-    res.json(
-        {
-            success:true,
-            message:"Actually Product Id from the files"
-        }
-    )
+exports.getProductId=async(req,res,next)=>{
+    try{
+        const product=await productModel.findById(req.params.id);
+        res.json(
+            {
+                success:true,
+                product
+            }
+        )
+    }catch(error){
+        res.status(404).json(
+            {
+                success:false,
+                message:"unable to get the product with the id"
+            }
+        )
+    }
 }
