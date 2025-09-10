f62f5c12bac514564edeb9075fb482b8450f9383
shushanth2003 <bsshu2003@gmail.com>
2025-09-10 10:44:26 +0530

    Feat: Push a Data

--- mini-ecommerce/backend/app.js
+++ mini-ecommerce/backend/app.js
@@ -7,6 +7,7 @@ const product=require("./router/product.js")
 const order=require("./router/order.js");
 const connectDB = require('./config/connectDB.js');
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1/",product);
 app.use("/api/v1/",order);
 connectDB();
--- mini-ecommerce/backend/controller/orderController.js
+++ mini-ecommerce/backend/controller/orderController.js
@@ -1,4 +1,7 @@
 exports.createProduct=(req,res,next)=>{
+    const cardItems=req.body;
+    const amount=cardItems.reduce((acc,items)=>{acc+items.product.price*items.product.qty},0);
+    console.log("Amount of Total Price : "+amount);
     res.json(
         {
             success:true,
